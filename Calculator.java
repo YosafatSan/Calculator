b1e09a967acf04e6c0af3fa39200a245fad7cebb
Yosafat Sanchez Leon <yosafat.sanchez@bigcommerce.com>
2018-09-06 11:00:16 -0500

    Added Icalculator class with Fibo 10

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,29 @@
 public class Calculator{
+
+    public static class ICalculator{
+        public void Fibo(){
+            System.out.println("Fibbonacci de 10" + CalcFibonacci(10));
+        }
+        private int CalcFibonacci(int n){
+            if (n>1){
+                return CalcFibonacci(n-1) + CalcFibonacci(n-2);  //función recursiva
+            }
+            else if (n==1) {  // caso base
+                return 1;
+            }
+            else if (n==0){  // caso base
+                return 0;
+            }
+            else{ //error
+                System.out.println("Debes ingresar un tamaño mayor o igual a 1");
+            return -1; 
+            }
+        }
+    }
+
+
     public static void main(String []arg){
-        System.out.println("Hola mundo");
+        ICalculator Calc = new ICalculator();
+        Calc.Fibo();
     }
 }
